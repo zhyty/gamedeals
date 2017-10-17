e409b456ce04ec8ef6d88f73819a5ce792756ef3
zhyty <tomyang711@gmail.com>
2017-10-17 19:25:03 -0400

    rename package to gamedeals

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='reddit_deals',
-    packages=['reddit_deals'],
+    packages=['gamedeals'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
 )
